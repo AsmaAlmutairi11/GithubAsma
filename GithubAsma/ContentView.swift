66a4d9f5b933da6f7e428ed8c455a5caa2631b40
Asma <Asmamutairi11@gmail.com>
2024-09-26 17:06:39 +0300

    Initial Commit

---
+++ GithubAsma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubAsma
+//
+//  Created by Asma Mohammed on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubAsma/GithubAsmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubAsmaApp.swift
+//  GithubAsma
+//
+//  Created by Asma Mohammed on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GithubAsmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
